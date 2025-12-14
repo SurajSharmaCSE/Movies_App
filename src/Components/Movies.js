7aaa8d33f3653e9d3026c0dd91cd7de8248d557e
Suraj Sharma <sharmasurajkumar4649@gmail.com>
2025-12-14 12:40:59 +0530

    API calling and changing DOM Acording to page click

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,13 +1,74 @@
 import React, {Component} from "react";
 import { movies } from "./getMovies"
+import axios from 'axios';
+
 
 export default class Navbar extends Component {
+    constructor()
+    {
+        super();
+        this.state={
+            hover:'',
+            parr:[1],
+            currPage:1,
+            movies:[]
+        }
+    }
+
+    async componentDidMount()
+    {
+        // side effects
+        const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currPage}`);
+        let data = res.data
+        // console.log(data);
+        this.setState({
+            movies:[...data.results]
+        })
+
+    }
+
+    changeMovies = async()=>{
+        const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currPage}`);
+        let data = res.data;
+        this.setState({
+            movies:[...data.results]
+        })
+    }
+
+    handleRight=()=>{
+        let temparr =[]
+        for(let i=1;i<=this.state.parr.length+1;i++){
+            temparr.push(i);
+        }
+        this.setState({
+            parr:[...temparr],
+            currPage:this.state.currPage+1
+        },this.changeMovies)
+    }
+
+    handleLeft=()=>{
+        if(this.state.currPage!=1){
+            this.setState({
+                currPage:this.state.currPage-1
+            },this.changeMovies)
+        }
+    }
+
+    handleClick=(value)=>{
+        if(value!=this.state.currPage){
+            console.log("i am called")
+            this.setState({
+                currPage:value
+            },this.changeMovies)
+        }
+    }
+
     render() {
-        let movie = movies.results;
+        // let movie = movies.results;
         return (
             <>
                 {
-                 movie.length == 0?
+                 this.state.movies.length == 0?
                         <div className="spinner-border text-primary" role="status">
                         <span className="visually-hidden">Loading...</span>
                         </div>:
@@ -15,8 +76,8 @@ export default class Navbar extends Component {
                             <h3 className="text-center"><strong>Trending</strong></h3>
                             <div className="movies-list">
                                 {
-                                    movie.map((movieObj)=>(
-                                        <div className="card movies-card">
+                                    this.state.movies.map((movieObj)=>(
+                                        <div className="card movies-card" onMouseEnter={()=>this.setState({hover:movieObj.id})} onMouseLeave={()=>this.setState({hover:''})}>
                                             <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} alt={movieObj.title} className="card-img-top movies-img"/>
                                             {/* <div className="card-body"> */}
                                             <h5 className="card-title movies-title">{movieObj.original_title}</h5>
@@ -24,7 +85,11 @@ export default class Navbar extends Component {
                                             {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
                                             {/* </div> */}
                                             <div className="button-wrapper " style={{display:"flex", width:"100%", justifyContent:"center"}}>
-                                                <a href="#" className="btn btn-primary movies-button">Add to Favourites</a>
+                                            {
+                                                    this.state.hover === movieObj.id && 
+                                                    <a className="btn btn-primary movies-button">Add to Favourites</a>
+                                            }
+                                            
                                             </div>
                                         </div>
                                     ))
@@ -34,11 +99,13 @@ export default class Navbar extends Component {
                            <div style={{display:'flex',justifyContent:'center'}}>
                                 <nav aria-label="Page navigation example">
                                     <ul class="pagination">
-                                        <li class="page-item"> <a class="page-link" href="a">Previous</a> </li>
-                                        <li class="page-item"><a class="page-link" href="a">1</a></li>
-                                        <li class="page-item"><a class="page-link" href="a">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="a">3</a></li>
-                                        <li class="page-item"><a class="page-link" href="a">Next</a></li>
+                                         <li class="page-item"> <a class="page-link" onClick={this.handleLeft}>Previous</a> </li>
+                                        {
+                                            this.state.parr.map((value)=>(
+                                                 <li class="page-item"><a class="page-link" onClick={()=>this.handleClick(value)} >{value}</a></li>
+                                            ))
+                                        }
+                                        <li class="page-item"><a class="page-link" onClick={this.handleRight}>Next</a></li>
                                     </ul>
                                 </nav>
                             </div>
